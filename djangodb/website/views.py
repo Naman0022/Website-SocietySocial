8e877e8b866c7741afef27777189a2eade29d1e1
Vayansh <vgarg3_be21@thapar.edu>
2022-02-26 16:47:01 +0530

    hg

--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -5,3 +5,5 @@ from .models import Member
 def home(request):
     all_member = Member.objects.all
     return render(request,'home.html', {'all' : all_member})
+def join(request):
+    return render(request,'join.html', {})
